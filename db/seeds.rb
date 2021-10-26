f025c9c4a133c2070591ba28cd0366f381dc61f9
jose alfredo <joalfredima@gmail.com>
2021-10-26 12:53:34 -0500

    Agregar campos y valores

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.create(username: "jdiaz", password: "temporal", rol: "ADMINISTRADOR")
+User.create(username: "jdiaz", password: "temporal", rol: "ADMINISTRADOR", name: "admin", email: "admin@example.com")
